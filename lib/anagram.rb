271ec22543da24cb9741720220c4da604bc3db6e
Daniel <dkilcullen25@gmail.com>
2022-02-01 12:20:18 -0600

    commit tree c7aedb42dd6eed543bf5ac6f416ff73561389c6f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize word
+        @word = word
+    end
+
+    def match array
+        a_array = @word.scan /\w/
+        array.select do |word|
+            b_array = word.scan /\w/
+            a_array.sort == b_array.sort
+        end
+    end
+end
+
+diaper = Anagram.new 'diaper'
+puts diaper.match %w[hello world zombies pants dipper]
